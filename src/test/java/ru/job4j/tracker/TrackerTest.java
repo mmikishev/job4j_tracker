f2ae0979f49836f63677ecd7d1f7d9a1d4bf395e
mmikishev <mmikishev@mail.ru>
2022-05-09 10:21:17 +0400

    2. Репозиторий под проект Tracker [#245623 #298955]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -6,6 +6,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class TrackerTest {
+
     @Test
     public void whenAddNewItemThenTrackerHasSameItem() {
         Tracker tracker = new Tracker();
