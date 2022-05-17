3b8118057734c2c12cdd9cf332543de413e0b9ed
mmikishev <mmikishev@mail.ru>
2022-05-17 18:43:50 +0400

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,26 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("активно " + active);
+        System.out.println("статус " + status);
+        System.out.println("сообщение " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 3, "ошибка");
+        error.printInfo();
+
+    }
+}
