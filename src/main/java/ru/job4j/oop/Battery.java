94d0b91a1e7abea15df21bd0aa66e0b4c2c9c8e8
mmikishev <mmikishev@mail.ru>
2022-05-18 17:35:08 +0400

    1.8. Состояние объекта [#167388 #302077]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -13,6 +13,7 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        this.load = this.load - load + another.load;
+        another.load += this.load;
+        this.load = 0;
     }
 }
