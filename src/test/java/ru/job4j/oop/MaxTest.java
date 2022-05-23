804bbafff2a712d933ce69c39dd1758083ffc119
mmikishev <mmikishev@mail.ru>
2022-05-23 19:55:02 +0400

    3. Перегрузить метод max для трех чисел [#122644]

---
+++ src/test/java/ru/job4j/oop/MaxTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.oop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.assertThat;
+
+public class MaxTest {
+
+    @Test
+    public void whenMax1To2Then2() {
+        int first = 1;
+        int second = 2;
+        int result = Max.max(first, second);
+        int expected = 2;
+        Assert.assertEquals(result, expected);
+    }
+}
