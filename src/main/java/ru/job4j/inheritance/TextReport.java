a73a5df2ff14783ee45b89219d46ac88fb3a19fc
mmikishev <mmikishev@mail.ru>
2022-05-28 15:19:44 +0400

    6. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/TextReport.java
+++ src/main/java/ru/job4j/inheritance/TextReport.java
@@ -1,6 +1,8 @@
 package ru.job4j.inheritance;
 
 public class TextReport {
+
+    @Override
     public String generate(String name, String body) {
         return name + System.lineSeparator() + body;
     }
