c4c5a0918fb9eca128b12504941adac79b99118b
mmikishev <mmikishev@mail.ru>
2022-05-29 19:39:40 +0400

    7. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,15 +1,12 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
 
     public Item() {
     }
@@ -37,5 +34,19 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+}
+
+class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+        LocalDateTime created = item.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String createdDateTimeFormat = created.format(formatter);
+        System.out.println(createdDateTimeFormat);
     }
 }
