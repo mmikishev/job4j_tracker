46b87e537882cdd6f82509a2cc65c8f1b6351b50
mmikishev <mmikishev@mail.ru>
2022-05-31 17:57:33 +0400

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,14 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFio("Maksim Mikishev");
+        student.setGroup(411);
+        student.getReceipt();
+
+        System.out.println(student.getFio() + " группа - " + student.getGroup());
+    }
+}
