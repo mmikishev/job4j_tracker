16770588741f6c7fe940465fd547ebc6a2cb5001
mmikishev <mmikishev@mail.ru>
2022-06-09 20:12:31 +0400

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,36 +14,20 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-      Item[] rsl = new Item[items.length];
-      int size = 0;
-        for (int index = 0; index < items.length; index++) {
-            Item item = items[index];
-            if (item != null) {
-                rsl[size] = item;
-                size++;
-            }
-        }
-        rsl = Arrays.copyOf(rsl, size);
-        for (int index = 0; index < rsl.length; index++) {
-            break;
-        }
-        return rsl;
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
-      Item[] rsl = new Item[items.length];
-      int size = 0;
-        for (int index = 0; index < items.length; index++) {
+      Item[] rsl = new Item[size];
+      int count = 0;
+        for (int index = 0; index < size; index++) {
             Item item = items[index];
-            if (item.equals(item.getName())) {
-                rsl[size] = item;
-                size++;
+            if (key.equals(item.getName())) {
+                rsl[count] = item;
+                count++;
             }
         }
         rsl = Arrays.copyOf(rsl, size);
-        for (int index = 0; index < rsl.length; index++) {
-            break;
-        }
         return rsl;
     }
 
