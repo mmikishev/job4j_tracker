c376cd108b88ddeb9b91fb5980b8918155f82eda
mmikishev <mmikishev@mail.ru>
2023-01-30 18:45:36 +0400

    9. Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -6,21 +6,21 @@ public class Broken {
 
     public String surname;
 
-    String name;
+    private String name;
 
-    Broken() { }
+    public Broken() { }
 
     public static final String NEWVALUE = "";
 
-    void echo() { }
+    public void echo() { }
 
-     void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
     }
 
 }
