8b47a38322db3dbe7263bfdeaaef0166b3e202b8
mmikishev <mmikishev@mail.ru>
2023-03-25 18:11:23 +0400

    1. Паспорт и жители [#212656]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -2,6 +2,7 @@ package ru.job4j.collection;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import  static org.junit.Assert.assertFalse;
 
 public class PassportOfficeTest {
     @Test
@@ -10,5 +11,14 @@ public class PassportOfficeTest {
         PassportOffice office = new PassportOffice();
         office.add(citizen);
         assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+    }
+
+    @Test
+    public void whenTestisFalseMethod() {
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen1);
+        assertFalse(office.add(citizen2));
     }
 }
